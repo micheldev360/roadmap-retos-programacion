8fc0cbdd5715c5350bd600c9ef0e87cbb06fcdf0
micheldev360 <michelbackup61@gmail.com>
2025-04-12 13:53:44 -0300

    Create micheldev360.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/micheldev360.py
@@ -0,0 +1,58 @@
+# https://python.org
+
+# Comentario en una linea 
+
+"""
+Esto tambien es un comentario pero
+en varias lineas, son simples anotaciones no ejecutan nada
+sobre el programa, en otros lenguajes puede existir o no
+"""
+
+'''
+Esto tambien es otra forma de crear comentarios, con comillas simples 
+'''
+
+"""
+Ahora vamos a crear una variable.
+Para crear una variables simplemente hay que asignarle un nombre a algo, para hacerlo en cada lenguaje
+primero hay que saber su sintaxis por defecto, como escribir cada cosa, ej: mayusculas, guiones bajos, etc.
+en python es el guion bajo, como una serpiente, por cada nueva palabra hay un guion bajo y todo se escribe en 
+minusculas.
+"""
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+# Esto es porque puedo cambiar las variables y sus valores por eso son variables.
+
+"""
+Ahora vamos a crear una constante, que en realidad en python no existen constantes, pero 
+se puede dejar asignado que es algo que no debe modificarse, si escribo la variable en mayusculas
+esto quiere decir que quiero que sea una constante.
+"""
+MY_CONSTANT = "Mi constante" # Por convención, se puede cambiar si alguien lo hace.
+
+"""
+Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...). En python existen 4 tipos de datos primitivos,
+ en otros lenguajes habra mas o menos.
+"""
+
+my_int = 1
+# Como lo puse sin comillas pasa a ser un numero entero
+
+my_float = 1.5
+# Asi se escribe un numero decimal
+
+my_bool = False
+my_bool = True
+# Esto atiende a logica booleana, algo que puede ser verdadero o falso
+
+my_string = "Mi cadena de texto"
+my_other_string= 'Mi otra cadena de texto'
+# Esto es lo que vimos al principio, se puede con comillas simples o dobles, pero nunca las dos juntas.
+
+
+
+
+
+
+
